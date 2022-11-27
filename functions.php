376f3f07cab485dd59f34b84e6c2613f18529d34
luiscarreno74 <luiscarreno1974@gmail.com>
2022-11-27 18:34:50 -0400

    add cpt and rest api endpoint

--- functions.php
+++ functions.php
@@ -33,7 +33,8 @@ function events_custom_post_type() {
      */
     $labels = array(
         'name'          => 'Events', // Plural name
-        'singular_name' => 'Event'   // Singular name
+        'singular_name' => 'Event',   // Singular name
+        'all_items' => 'All Events' //Todos los eventos
     );
 
     /*
@@ -56,9 +57,9 @@ function events_custom_post_type() {
      */
     $args = array(
         'labels'              => $labels,
-        'description'         => 'Post type post product', // Description
+        'description'         => 'Football Events in the 2022 World Cup', // Description
         'supports'            => $supports,
-        'taxonomies'          => array( 'category', 'post_tag' ), // Allowed taxonomies
+        'taxonomies'          => array( 'category'), // Allowed taxonomies
         'hierarchical'        => false, // Allows hierarchical categorization, if set to false, the Custom Post Type will behave like Post, else it will behave like Page
         'public'              => true,  // Makes the post type public
         'show_ui'             => true,  // Displays an interface for this post type
@@ -66,7 +67,7 @@ function events_custom_post_type() {
         'show_in_nav_menus'   => true,  // Displays in Appearance -> Menus
         'show_in_admin_bar'   => true,  // Displays in the black admin bar
         'menu_position'       => 5,     // The position number in the left menu
-        'menu_icon'           => true,  // The URL for the icon used for this post type
+        'menu_icon'           => 'dashicons-tickets-alt',  // The URL for the icon used for this post type
         'can_export'          => true,  // Allows content export using Tools -> Export
         'has_archive'         => true,  // Enables post type archive (by month, date, or year)
         'exclude_from_search' => false, // Excludes posts of this type in the front-end search result page if set to true, include them if set to false
@@ -74,6 +75,33 @@ function events_custom_post_type() {
         'capability_type'     => 'post' // Allows read, edit, delete like “Post”
     );
 
-    register_post_type('Event', $args); //Create a post type with the slug is ‘product’ and arguments in $args.
+    register_post_type('Event', $args); //Create a post type with the slug is ‘Event’ and arguments in $args.
 }
 add_action('init', 'events_custom_post_type');
+
+add_action( 'rest_api_init', function () {
+    register_rest_route( 'luiscarreno/v1', 'eventos', array(
+      'methods' => 'GET',
+      'callback' => 'events_rest_handler'
+    ) );
+  } );
+
+  function events_rest_handler($parametros){
+
+	//array use in the WP_QUERY
+	$args = array(
+		'post_type' => 'Events',
+		'post_status' => 'publish',
+		'posts_per_page' => -1,
+	);
+	$Events = new WP_Query( $args );
+
+	//adding thumbnail and permalink for each event by luis carreno 
+	$array_Events = array();
+
+	foreach($Events->posts as $Event){
+		array_push($array_Events,array('Image' => get_the_post_thumbnail_url($Event->ID),'content' => $Event->post_content,'title' => $Event->post_title, 'link' => get_permalink($Event->ID)));
+	}
+
+	return array($array_Events);
+}
